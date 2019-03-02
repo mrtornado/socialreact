7d8d6ed2ec437feb5a9440cf89851a33aff8c12b
Toro <toro@Torel.local>
2019-03-03 01:18:59 +0200

    all files

---
+++ src/post/NewPost.js
@@ -0,0 +1,168 @@
+import React, { Component } from "react";
+import { isAuthenticated } from "../auth";
+import { create } from "./apiPost";
+import { Redirect } from "react-router-dom";
+
+class NewPost extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: "",
+      body: "",
+      photo: "",
+      error: "",
+      user: {},
+      fileSize: 0,
+      loading: false,
+      redirectToProfile: false
+    };
+  }
+
+  componentDidMount() {
+    this.postData = new FormData();
+    this.setState({ user: isAuthenticated().user });
+  }
+
+  isValid = () => {
+    const { title, body, fileSize } = this.state;
+    this.setState({ loading: false });
+
+    if (fileSize > 1500000) {
+      this.setState({ error: "Image file should not be larger then 1.5mb" });
+      return false;
+    }
+
+    if (title.length === 0) {
+      this.setState({ error: "A title is required" });
+      return false;
+    }
+
+    if (body.length < 90) {
+      this.setState({
+        error: "The post body should be at least 90 characters long"
+      });
+      return false;
+    }
+
+    if (body.length === 0) {
+      this.setState({ error: "A post body is required" });
+      return false;
+    }
+
+    return true;
+  };
+
+  handleChange = (name) => (event) => {
+    this.setState({ error: "" });
+
+    const value = name === "photo" ? event.target.files[0] : event.target.value;
+    const fileSize = name === "photo" ? event.target.files[0].size : 0;
+
+    this.postData.set(name, value);
+    this.setState({ error: "" });
+    this.setState({ [name]: value });
+    this.setState({ [name]: value, fileSize });
+  };
+
+  clickSubmit = (event) => {
+    event.preventDefault();
+    this.setState({ loading: true });
+
+    if (this.isValid()) {
+      const userId = isAuthenticated().user._id;
+      const token = isAuthenticated().token;
+
+      create(userId, token, this.postData).then((data) => {
+        if (data.error) this.setState({ error: data.error });
+        else {
+          this.setState({
+            loading: false,
+            title: "",
+            body: "",
+            photo: "",
+            redirectToProfile: true
+          });
+        }
+      });
+    }
+  };
+
+  NewPostForm = (title, body) => (
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Photo</label>
+        <input
+          onChange={this.handleChange("photo")}
+          type="file"
+          accept="image/*"
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Title</label>
+        <input
+          onChange={this.handleChange("title")}
+          type="text"
+          className="form-control"
+          value={title}
+        />
+      </div>
+
+      <div className="form-group">
+        <label className="text-muted">Body</label>
+        <i className="fas fa-pencil-alt prefix" />
+        <textarea
+          onChange={this.handleChange("body")}
+          type="text"
+          className="md-textarea form-control"
+          value={body}
+        />
+      </div>
+
+      <button onClick={this.clickSubmit} className="btn btn-raised btn-primary">
+        Create Post
+      </button>
+    </form>
+  );
+
+  render() {
+    const {
+      title,
+      body,
+      // photo,
+      user,
+      error,
+      loading,
+      redirectToProfile
+    } = this.state;
+
+    if (redirectToProfile) {
+      return <Redirect to={`/user/${user._id}`} />;
+    }
+
+    return (
+      <div className="container">
+        <h2 className="mt-5 mb-5">Create a new post</h2>
+
+        <div
+          className="alert alert-danger"
+          style={{ display: error ? "" : "none" }}
+        >
+          {error}
+        </div>
+
+        {loading ? (
+          <div className="jumbotron text-center">
+            <h2>Loading...</h2>
+          </div>
+        ) : (
+          ""
+        )}
+
+        {this.NewPostForm(title, body)}
+      </div>
+    );
+  }
+}
+
+export default NewPost;
---
+++ src/post/Posts.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { list } from "./apiPost";
+import DefaultPosts from "../images/loading.jpg";
+import { truncate } from "lodash";
+
+class Posts extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    list().then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        this.setState({ posts: data });
+      }
+    });
+  }
+
+  renderPosts = (posts) => {
+    return (
+      <div className="row">
+        {posts.map((post, i) => {
+          const posterId = post.postedBy ? `/user/${post.postedBy._id} ` : "";
+          const posterName = post.postedBy ? post.postedBy.name : "Unknown";
+
+          return (
+            <div className="card col-md-4 mb-3" key={i}>
+              <div className="card-body">
+                <img
+                  src={`${process.env.REACT_APP_API_URL}/post/photo/${
+                    post._id
+                  }`}
+                  alt={post.title}
+                  onError={(i) => (i.target.src = `${DefaultPosts}`)}
+                  className="img-thumbnail mb-3"
+                  style={{ height: "200px", width: "100%" }}
+                />
+                <h5 className="card-title">
+                  {truncate(post.title, { length: 25, separator: /,?\.* +/ })}
+                </h5>
+                <p className="card-text">
+                  {truncate(post.body, { length: 90, separator: /,?\.* +/ })}
+                </p>
+                <br />
+                <p className="font-italic mark">
+                  Posted by <Link to={`${posterId}`}>{posterName} </Link>
+                  on {new Date(post.created).toDateString()}
+                </p>
+                <Link
+                  to={`/post/${post._id}`}
+                  className="btn btn-raised btn-sm btn-primary"
+                >
+                  Read more
+                </Link>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <div className="container">
+        <h2 className="mt-5 mb-5">
+          {posts.lenght ? "Loading..." : "Recent posts"}
+        </h2>
+        {this.renderPosts(posts)}
+      </div>
+    );
+  }
+}
+
+export default Posts;
